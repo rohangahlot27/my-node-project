f92311714b4729f8035b23f9e353a829aa3b9461
rohangahlot27 <rohan.gahlot27@gmail.com>
2025-07-13 12:59:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-// index.js
-console.log("Hello from my Node.js project!");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Render deployment!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
